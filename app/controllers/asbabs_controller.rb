9ea3a6704a3356dce05f0d741305a02a372244cf
vita <dragic.hsieh@gmail.com>
2018-09-04 19:40:16 +0800

    Done add asbab index action and view for public users

---
+++ app/controllers/asbabs_controller.rb
@@ -0,0 +1,4 @@
+class AsbabsController < ApplicationController
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	# 前台
+  root "asbabs#index"
 end

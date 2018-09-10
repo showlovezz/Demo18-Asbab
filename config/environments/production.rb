8e9e33b581abd52f9e612ca9087a94ea1afb0255
vita <dragic.hsieh@gmail.com>
2018-09-10 11:24:11 +0800

    Done add system send confirmable email to users for sign_up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #==============================
+  # email
+  #==============================
+  # for email
+  config.action_mailer.default_url_options = {protocol: 'http', host: 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
 end

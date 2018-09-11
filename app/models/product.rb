e3e6d019a7ae160d4650f07ba7aab788f10fe433
vita <dragic.hsieh@gmail.com>
2018-09-12 03:24:20 +0800

    Done link category and product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,10 @@
 class Product < ApplicationRecord
+	# 資料驗證必輸入
 	validates :name, :tag, :description, :basic_price, :special_price , presence: true
 
+	# 產品檔案下載器
 	mount_uploader :image, PhotoUploader
+
+	# 很多產品會屬於一個分類
+	belongs_to :category, optional: true
 end

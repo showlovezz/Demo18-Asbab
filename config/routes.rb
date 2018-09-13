755a8a29d1898f4f7b121fecb8fcadabe4398a8d
vita <dragic.hsieh@gmail.com>
2018-09-13 15:46:13 +0800

    Done setup product resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
 	# 前台網址設計
   root "asbabs#index"
+  resources :products, only: [:index, :show]
 
   # 後台網址設計
   namespace :admin do
     root "asbabs#index"
-    resources :products
+    resources :products, except: [:show]
     resources :categories
   end
 end

0d135c1ab8d9db0e3ed30c114ad3c6a951d2e034
vita <dragic.hsieh@gmail.com>
2018-09-15 01:24:04 +0800

    Done add show page for public users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+	def show
+		@product = Product.find(params[:id])
+		@random_product = Product.where.not(id: @product.id).order("RANDOM()").limit(4)
+	end
 end

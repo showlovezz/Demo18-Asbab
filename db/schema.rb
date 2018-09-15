32fbd50403e66c440b4e69397dcea8e8f6903c0a
vita <dragic.hsieh@gmail.com>
2018-09-16 03:47:44 +0800

    Done create post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_11_190728) do
+ActiveRecord::Schema.define(version: 2018_09_15_194549) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

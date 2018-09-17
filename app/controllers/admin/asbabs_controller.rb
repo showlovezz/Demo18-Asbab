3b89ca56befed1e124c9afc37b5ad83947c3e235
vita <dragic.hsieh@gmail.com>
2018-09-18 04:02:14 +0800

    Done add count for admin

--- app/controllers/admin/asbabs_controller.rb
+++ app/controllers/admin/asbabs_controller.rb
@@ -1,5 +1,12 @@
 class Admin::AsbabsController < Admin::BaseController
 	def index
+		# 需要抓出 product.count 數量
 		@products = Product.all
+
+		# 需要抓出 post.count 數量
+		@posts = Post.all
+
+		# 需要抓出 user.count 數量
+		@users = User.all
 	end
 end

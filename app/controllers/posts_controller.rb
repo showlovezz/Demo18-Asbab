e8a7263b9429ae90ab61a61fc5ee0ea1279d1791
vita <dragic.hsieh@gmail.com>
2018-09-17 21:59:09 +0800

    Done add post show page for public users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+	def show
+		# 秀出特定的 Post 文章
+		@post = Post.find(params[:id])
+
+		# 配合導覽列需秀出全部的產品分類
+		@categories = Category.all
+
+		# 首頁秀出最新文章
+		@posts = Post.order("created_at DESC").limit(5)
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def verbose_data(data)
+		data.strftime("%B %d, %Y")
+	end
 end

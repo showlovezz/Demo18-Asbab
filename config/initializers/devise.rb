b182b9da73251b6038bd73d16c7d9572e9543bd0
vita <dragic.hsieh@gmail.com>
2018-09-19 02:11:49 +0800

    Debug Devise gem wrong message

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,6 +20,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.secret_key = '<%= ENV["SECRET_KEY_BASE"] %>' if Rails.env == 'production'
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
